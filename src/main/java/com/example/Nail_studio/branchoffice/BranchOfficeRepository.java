1f74ef6865edc488df5c1508b621c5b2e859687b
reuven9632 <reuven9632@gmail.com>
2022-12-24 22:09:12 +0200

    some changes

--- src/main/java/com/example/Nail_studio/branchoffice/BranchOfficeRepository.java
+++ src/main/java/com/example/Nail_studio/branchoffice/BranchOfficeRepository.java
@@ -10,8 +10,8 @@ import java.util.Optional;
 @Repository
 public interface BranchOfficeRepository extends JpaRepository<BranchOffice, Long> {
 
-
     List<BranchOffice> findAll();
+
     /*@Query("DELETE FROM Specialist s WHERE id = ?1")
     void deleteById(Long id);*/
 }
