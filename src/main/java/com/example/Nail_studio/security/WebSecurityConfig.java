ac88e7229bbeab9dd5d41b0f5e8410c8181da8f0
reuven9632 <reuven9632@gmail.com>
2023-01-08 22:17:42 +0200

    fix error with getting permissions on WebSecurityConfig

--- src/main/java/com/example/Nail_studio/security/WebSecurityConfig.java
+++ src/main/java/com/example/Nail_studio/security/WebSecurityConfig.java
@@ -24,8 +24,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/", "/a", "/css/**", "/js/**", "/images/**", "/fonts/**").permitAll()
                 .antMatchers("/admin/**").hasRole(Role.ADMINISTRATOR.name())
                 .antMatchers(HttpMethod.GET, "/management/**").hasAnyRole(Role.SPECIALIST.name(), Role.ADMINISTRATOR.name())
-                .antMatchers(HttpMethod.POST, "/management/**").hasAuthority(Permissions.DIARY_WRITE.name())
-                .antMatchers(HttpMethod.DELETE, "/management/**").hasAnyAuthority(Permissions.ORDER_DROP.name(), Permissions.OPTION_EXTRA.name())  // TODO: 1/8/2023   check if I steel need this option  -> Permissions.OPTION_EXTRA.name()    before production 
+                .antMatchers(HttpMethod.POST, "/management/**").hasAuthority(Permissions.DIARY_WRITE.getPermission())
+                .antMatchers(HttpMethod.DELETE, "/management/**").hasAnyAuthority(Permissions.ORDER_DROP.getPermission(), Permissions.OPTION_EXTRA.getPermission())  // TODO: 1/8/2023   check if I steel need this option  -> Permissions.OPTION_EXTRA.name()    before production
                 .anyRequest()
                 .authenticated()
                 .and()
